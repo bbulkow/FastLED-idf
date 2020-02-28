63d966b6f51eef0fba56e74cab01d7f7983d08a8
Brian Bulkowski <brian@bulkowski.org>
2020-02-28 01:29:35 -0800

    Removing unnecessary files

--- components/FastLED-idf/hal/esp32-hal.h
+++ components/FastLED-idf/hal/esp32-hal.h
@@ -61,9 +61,9 @@ void yield(void);
 // #include "esp32-hal-ledc.h"
 #include "esp32-hal-rmt.h"
 // #include "esp32-hal-sigmadelta.h"
-#include "esp32-hal-timer.h"
+// #include "esp32-hal-timer.h"
 // #include "esp32-hal-bt.h"
-#include "esp32-hal-psram.h"
+// #include "esp32-hal-psram.h"
 #include "esp32-hal-cpu.h"
 
 #ifndef BOARD_HAS_PSRAM
