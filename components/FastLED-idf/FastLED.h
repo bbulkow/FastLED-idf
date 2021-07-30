d36effcc490a025f29eb46d20757921a079b8083
J.R. Luppold <j.r.luppold@mattel.com>
2021-07-30 13:51:52 -0700

    Use RMT not I2S to drive neopixels

--- components/FastLED-idf/FastLED.h
+++ components/FastLED-idf/FastLED.h
@@ -10,7 +10,7 @@
 
 // prefer I2S? Comment this in.
 // Not the default because haven't tried it as much, does work
-#define FASTLED_ESP32_I2S
+//#define FASTLED_ESP32_I2S
 
 #include "esp32-hal.h"
 
